71afa2e7b55a6f723d5a5b7d5353d08671a596fb
MrZuzi <maratkhamza03@gmail.com>
2023-01-27 15:41:10 +0600

    lab1

--- tsis1/Data Types.py
+++ tsis1/Data Types.py
@@ -1,20 +1,81 @@
 x = 5
-print (type(x)) #int
+print(type(x))
 
-x= 'Hello world'
-print (type(x)) #str
+#Getting the data type
+x = 5
+print(type(x))
 
+#STR
+x = "Hello World"
+print(x)
+print(type(x)) 
+
+#INT
+x = 20
+print(x)
+print(type(x)) 
+
+#FLOAT
 x = 20.5
-print (type(x)) #float
+print(x)
+print(type(x)) 
 
+#COMPLEX
+x = 1j
+print(x)
+print(type(x)) 
+
+#LIST
 x = ["apple", "banana", "cherry"]
-print(type(x)) #list
+print(x)
+print(type(x)) 
 
+#TUPLE
 x = ("apple", "banana", "cherry")
-print(type(x)) #tuple
+print(x)
+print(type(x)) 
 
+#RANGE
+x = range(6)
+print(x)
+print(type(x)) 
+
+#DICT
 x = {"name" : "John", "age" : 36}
-print(type(x)) #dict
+print(x)
+print(type(x)) 
 
+#SET
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x)) 
+
+#FROZENSET
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x)) 
+
+#BOOL
 x = True
-print(type(x)) #bool
+print(x)
+print(type(x)) 
+
+#BYTES
+x = b"Hello"
+print(x)
+print(type(x)) 
+
+#BYTEARRAY
+x = bytearray(5)
+print(x)
+print(type(x)) 
+
+#MEMORYVIEW
+x = memoryview(bytes(5))	
+print(x)
+print(type(x)) 
+
+#NONE TYPE
+x = None	
+print(x)
+print(type(x)) 
